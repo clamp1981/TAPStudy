f163544c07123e7112bf4d5c24568dfec3c72038
clamp1981 <clamp1981@gmail.com>
2022-06-12 17:53:43 +0900

    TAP 프로젝트 생성

---
+++ TAPStudy/CalculateFactorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TAPStudy
+{
+    public class CalculateFactorial
+    {
+		public BigInteger Calculate(int input)
+		{
+			BigInteger output = 1;
+			for (int i = 0; i < input; i++)
+			{
+				output *= (i + 1);
+				Thread.Sleep(100);
+			}
+			Console.WriteLine($"CalculateFactorial : {output}");
+
+			return output;
+		}
+	}
+}
---
+++ TAPStudy/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAPStudy
+{
+    public partial class Form1 : Form
+    {
+        CalculateFactorial cal = new CalculateFactorial();
+        public Form1()
+        {
+            InitializeComponent();
+
+            for( int i = 10; i <= 100; i += 10 )
+            {
+                AddListViewItem(i);
+            }
+        }
+
+
+        private ListViewItem AddListViewItem(int testNumber)
+        {
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = testNumber.ToString();
+            lvi.SubItems.Add("Not Started");
+            lvi.SubItems.Add("1");            
+            lvi.SubItems.Add("---");
+            lvi.SubItems.Add("---");           
+
+            this.listView1.Items.Add(lvi);
+
+            return lvi;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tasks = new Task<BigInteger>[this.listView1.Items.Count];
+            for (var i = 0; i < this.listView1.Items.Count; i++)
+            {
+                int target = Convert.ToInt32(this.listView1.Items[i].SubItems[0].Text);
+                var task = new Task<BigInteger>(() => cal.Calculate((int)target));
+                tasks[i] = task;
+
+            }
+
+            foreach (var task in tasks)
+            {
+                task.Start();
+            }
+
+            Task.WaitAll(tasks, 10000);
+            Console.WriteLine("All task is done.");
+
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                this.listView1.Items[i].SubItems[3].Text = tasks[i].Result.ToString();
+
+            }
+
+
+
+
+        }
+    }
+}
